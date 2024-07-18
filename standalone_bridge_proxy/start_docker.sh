83f981700af37c9cc31a4eae973ae7e3ee285e96
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-07-18 14:14:55 +0200

    Config proxy

--- standalone_bridge_proxy/start_docker.sh
+++ standalone_bridge_proxy/start_docker.sh
@@ -6,5 +6,8 @@ docker run \
   -e HAPROXY_VERSION=2.9.7 \
   -e HAPROXY_URL=https://www.haproxy.org/download/2.9/src/haproxy-2.9.7.tar.gz \
   -e HAPROXY_SHA256=d1a0a56f008a8d2f007bc0c37df6b2952520d1f4dde33b8d3802710e5158c131 \
+  -it \
   --hostname zkevm-bridge-proxy-001-no-bs \
+  --name manual-proxy \
+  haproxy:2.9.7 \
   haproxy -f /usr/local/etc/haproxy/haproxy.cfg
