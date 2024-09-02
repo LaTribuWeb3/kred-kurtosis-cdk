28671f42c1a2442a9d0065152f7af61f265d26f3
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-09-02 17:19:59 +0200

    Install go and polycli

--- configure.sh
+++ configure.sh
@@ -40,3 +40,25 @@ if ! command -v kurtosis >/dev/null 2>&1; then
   sudo apt-get install -y kurtosis-cli
 fi
 
+if ! command -v go >/dev/null 2>&1; then
+  # Install Go
+  apt update
+  apt install -y wget
+
+  wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
+  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
+  export PATH=$PATH:/usr/local/go/bin
+  echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+
+  # Verify Go installation
+  go version
+fi
+
+if ! command -v polycli >/dev/null 2>&1; then
+# Original script content
+  git clone https://github.com/maticnetwork/polygon-cli.git
+  cd polygon-cli
+
+  make install
+  export PATH="$HOME/go/bin:$PATH"
+fi
